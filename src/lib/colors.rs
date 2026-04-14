de98cbd60c13f4c27462259cc1f713ab86bc3852
MysticalMike60t <121118774+MysticalMike60t@users.noreply.github.com>
2026-04-14 04:09:08 -0700

    Updated Versions

--- src/lib/colors.rs
+++ src/lib/colors.rs
@@ -1,20 +1,16 @@
 pub struct Rgb<'r, 'g, 'b> {
     r: &'r u8,
     g: &'g u8,
-    b: &'b u8
+    b: &'b u8,
 }
 
 impl<'r, 'g, 'b> Rgb<'r, 'g, 'b> {
     pub fn new(r: &'r u8, g: &'g u8, b: &'b u8) -> Self {
-        Self {
-            r: r,
-            g: g,
-            b: b
-        }
+        Self { r: r, g: g, b: b }
     }
     pub fn insert<'s>(self) -> &'s str {
         let ansi: String = format!("{};{};{}", self.r, self.g, self.b);
-        return (ansi.clone()).leak()
+        return (ansi.clone()).leak();
     }
     pub fn set_rgb(self, r: &'r u8, g: &'g u8, b: &'b u8) -> Rgb<'r, 'g, 'b> {
         let rgb: Rgb<'r, 'g, 'b> = Rgb::new(&r, &g, &b);
--- src/lib/prefixes.rs
+++ src/lib/prefixes.rs
@@ -2,7 +2,7 @@ use crate::colors::Rgb;
 
 pub struct Prefix<'rgb, 'display_text> {
     rgb: Rgb<'rgb, 'rgb, 'rgb>,
-    display_text: &'display_text str
+    display_text: &'display_text str,
 }
 
 impl<'rgb, 'display_text> Prefix<'rgb, 'display_text> {
@@ -13,14 +13,18 @@ impl<'rgb, 'display_text> Prefix<'rgb, 'display_text> {
         }
     }
     pub fn insert<'output>(self) -> &'output str {
-        let ansi: String = format!("\x1b[38;2;60;60;60m<\x1b[0m \x1b[38;2;{}m{}\x1b[0m \x1b[38;2;60;60;60m>\x1b[0m", self.rgb.insert(), self.display_text);
-        return (ansi.clone()).leak()
+        let ansi: String = format!(
+            "\x1b[38;2;60;60;60m<\x1b[0m \x1b[38;2;{}m{}\x1b[0m \x1b[38;2;60;60;60m>\x1b[0m",
+            self.rgb.insert(),
+            self.display_text
+        );
+        return (ansi.clone()).leak();
     }
     pub fn return_type<'output>(display_text: &'display_text str) -> &'output str {
         if display_text.to_lowercase().as_str().contains("success") {
             return "SUCCESS";
         } else {
-            return "NONE"
+            return "NONE";
         }
     }
     pub fn return_rgb<'output>(display_text: &'display_text str) -> Rgb<'output, 'output, 'output> {
@@ -28,13 +32,13 @@ impl<'rgb, 'display_text> Prefix<'rgb, 'display_text> {
         let g: &u8;
         let b: &u8;
         if display_text.to_lowercase().as_str().contains("success") {
-            r=&140;
-            g=&240;
-            b=&140;
+            r = &140;
+            g = &240;
+            b = &140;
         } else {
-            r=&60;
-            g=&60;
-            b=&60;
+            r = &60;
+            g = &60;
+            b = &60;
         }
         let rgb: Rgb<'output, 'output, 'output> = Rgb::new(&r, &g, &b);
         return rgb;
